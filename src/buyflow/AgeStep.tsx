514f1b18f71760c6b80d992edad70059af1ff70f
Swathi-ShivarameGowda <85452976+Swathi-ShivarameGowda@users.noreply.github.com>
2021-08-26 16:50:12 +0200

    Making other fields as required

--- src/buyflow/AgeStep.tsx
+++ src/buyflow/AgeStep.tsx
@@ -1,26 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 interface AgeStepProps {
-  cb: (field: string, value: number) => void
+  cb: (field: string, value: number) => void;
 }
 
 const AgeStep: React.FC<AgeStepProps> = (props) => {
-  const [age, setAge] = useState(0)
+  const [age, setAge] = useState(0);
+  const [error, setError] = useState("");
+  const handleClick = () => {
+    return age
+      ? props.cb("age", age)
+      : setError("please enter the age in the above field to continue");
+  };
   return (
     <>
       <div>
-        Age:{' '}
+        Age:{" "}
         <input
           type="number"
           onChange={({ target: { value } }) => {
-            setAge(Number(value))
+            setAge(Number(value));
           }}
           value={age}
         ></input>
       </div>
-      <button onClick={() => props.cb('age', age)}>Next</button>
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Next</button>
     </>
-  )
-}
+  );
+};
 
-export default AgeStep
+export default AgeStep;
--- src/buyflow/EmailStep.tsx
+++ src/buyflow/EmailStep.tsx
@@ -1,26 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 interface EmailStepProps {
-  cb: (field: string, value: string) => void
+  cb: (field: string, value: string) => void;
 }
 
 const EmailStep: React.FC<EmailStepProps> = (props) => {
-  const [email, setEmail] = useState('')
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const handleClick = () => {
+    return email
+      ? props.cb("email", email)
+      : setError("please enter the email in the above field to continue");
+  };
   return (
     <>
       <div>
-        Email:{' '}
+        Email:{" "}
         <input
           type="email"
           onChange={({ target: { value } }) => {
-            setEmail(value)
+            setEmail(value);
           }}
           value={email}
         ></input>
       </div>
-      <button onClick={() => props.cb('email', email)}>Next</button>
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Next</button>
     </>
-  )
-}
+  );
+};
 
-export default EmailStep
+export default EmailStep;
