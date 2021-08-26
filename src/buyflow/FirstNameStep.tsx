7af82b553cd87cdace251e86a899ca0ccd9c9df3
Swathi-ShivarameGowda <85452976+Swathi-ShivarameGowda@users.noreply.github.com>
2021-08-26 16:31:44 +0200

    initial commit

---
+++ src/buyflow/FirstNameStep.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+interface FirstNameStepProps {
+  cb: (field: string, value: string) => void;
+}
+
+const FirstNameStep: React.FC<FirstNameStepProps> = (props) => {
+  const [firstName, setFirstName] = useState("");
+  return (
+    <>
+      <div>
+        First Name:{" "}
+        <input
+          type="firstName"
+          onChange={({ target: { value } }) => {
+            setFirstName(value);
+          }}
+          value={firstName}
+        ></input>
+      </div>
+      <button onClick={() => props.cb("firstName", firstName)}>Next</button>
+    </>
+  );
+};
+
+export default FirstNameStep;
